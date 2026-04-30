bcfe04288fdf2651d5374d07ca083a570535cffe
local-bit-tech <yassa.rasil@gmail.com>
2026-04-30 12:08:26 -0400

    Add comments for key generation process
    Added comments to clarify the purpose of the time interval and key generation loop.

--- Task 2/genkey.c
+++ Task 2/genkey.c
@@ -6,14 +6,16 @@ void main()
 {
     int i;
 
+    //time from start to finish of interval where key is 
     long long start_time = 1524013729;
     long long end_time = 1524020929;
 
 
     char key[KEYSIZE];
 
-    
+    //loop through the time from start to finish generating a key
     for (start_time; start_time <= end_time; start_time++){
+        //seed a new number from each time
         srand(start_time);
         for (i = 0; i < KEYSIZE; i++){
             key[i] = rand()%256;
